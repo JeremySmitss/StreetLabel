ee04be3829e22c1b395246fdeebe76369d9ba408
JeremySmits <97687791+JeremySmitss@users.noreply.github.com>
2022-11-08 15:09:24 +0100

    Add files via upload

---
+++ configuration.lua
@@ -0,0 +1,15 @@
+config = {
+	-- Customization options
+	color = '#FFFFFF';
+
+	position = {
+		-- 0:100
+		offsetX = 15;
+		offsetY = 0.5; 
+	};
+
+  scale = 0.9;
+
+	vehicleCheck = true; -- Rather or not to display HUD when player(s) are inside a vehicle
+  dynamic = false;
+}
